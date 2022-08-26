798bbe581710ffb5a6864583196d19134fef5295
nandu <parmanandpally@gmail.com>
2022-08-26 12:01:56 +0530

    added AreaOfrectangle

--- MyGitHub_ConsoleApp/Program.cs
+++ MyGitHub_ConsoleApp/Program.cs
@@ -12,6 +12,11 @@ namespace MyGitHub_ConsoleApp
         {
             Console.WriteLine("C# Console App Project");
             Console.WriteLine("Implemented using GitHub Repository");
+            Console.WriteLine("Area = {0}",AreaOfRectangle(12,22));
+        }
+        static double AreaOfRectangle(double l, double b)
+        {
+            return l * b;
         }
     }
 }
